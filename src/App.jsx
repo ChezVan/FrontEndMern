9cea87a394600b0df03cc9b15fe9d64a84148536
ChezVan <chezney@thedigitalacademy.co.za>
2023-07-15 15:10:56 +0200

    front end MERN project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// "as" mean variable 
+import {BrowserRouter as Router} from 'react-router-dom'
+
+const App = () => {
+  return (
+    
+    <Router>
+      
+    </Router>
+
+  )
+}
+
+export default App
